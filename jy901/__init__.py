05e45d720e383cd9f41a026fd1de720912589300
Marc Poulhiès <github@kataplop.net>
2018-04-15 16:38:43 +0200

    support for gyro + live 3D display
    Initial support for gyroscope readings. Live 3D view is still in early
    dev, we may need to filter sensor value.
    There is still an obvious problem with the rotation matrix as
    acceleration at rest is not always along Z (after projection) axis.

--- jy901/__init__.py
+++ jy901/__init__.py
@@ -1,6 +1,106 @@
 import math
 import numpy.linalg
 
+import numpy as np
+import math
+
+class AccelFrame:
+    def __init__(self, byts):
+        a = np.array([int.from_bytes(byts[2:4], byteorder='little', signed=True),
+                      int.from_bytes(byts[4:6], byteorder='little', signed=True),
+                      int.from_bytes(byts[6:8], byteorder='little', signed=True)])
+
+        self.accel = (a/32768)*16
+
+        self.temp = int.from_bytes(byts[8:10], byteorder='little', signed=True) /100
+
+        self.frame = byts
+
+    def get_norm(self):
+        return numpy.linalg.norm(self.accel)
+
+    def __str__(self):
+        n = self.get_norm()
+        return "acc=({0:10.4f}, {1:10.4f}, {2:10.4f})~{3:10.4f}, temp={4:10.4f}".format(self.accel[0], self.accel[1], self.accel[2], n, self.temp)
+
+
+class AngleFrame:
+    def __init__(self, byts):
+        self.roll = int.from_bytes(byts[2:4], byteorder='little', signed=True) / 32768
+        self.pitch = int.from_bytes(byts[4:6], byteorder='little', signed=True) / 32768
+        self.yaw = int.from_bytes(byts[6:8], byteorder='little', signed=True) / 32768
+
+        self.temp = int.from_bytes(byts[8:10], byteorder='little', signed=True) /100
+
+        self.frame = byts
+
+    def __str__(self):
+        return "angle=(roll:{:10.4f}, pitch:{:10.4f}, yaw:{:10.4f}), temp={:10.4f}".format(self.roll, self.pitch, self.yaw, self.temp)
+
+    def get_rotation_matrix(self):
+        yawMatrix = np.array([
+            [math.cos(self.yaw), -math.sin(self.yaw), 0],
+            [math.sin(self.yaw), math.cos(self.yaw), 0],
+            [0, 0, 1]
+        ])
+
+        pitchMatrix = np.array([
+            [math.cos(self.pitch), 0, math.sin(self.pitch)],
+            [0, 1, 0],
+            [-math.sin(self.pitch), 0, math.cos(self.pitch)]
+        ])
+
+        rollMatrix = np.array([
+            [1, 0, 0],
+            [0, math.cos(self.roll), -math.sin(self.roll)],
+            [0, math.sin(self.roll), math.cos(self.roll)]
+        ])
+        return yawMatrix @ pitchMatrix @ rollMatrix
+
+class MagneticFrame:
+    def __init__(self, byts):
+        self.h = [int.from_bytes(byts[2:4], byteorder='little', signed=True),
+                  int.from_bytes(byts[4:6], byteorder='little', signed=True),
+                  int.from_bytes(byts[6:8], byteorder='little', signed=True)]
+
+        self.temp = int.from_bytes(byts[8:10], byteorder='little', signed=True) /100
+
+        self.frame = byts
+
+    def __str__(self):
+        return "magnetic H=({:10.4f}, {:10.4f}, {:10.4f}), temp={:10.4f}".format(self.h[0], self.h[1], self.h[2], self.temp)
+
+class AngularVelocityFrame:
+    def __init__(self, byts):
+        w = [int.from_bytes(byts[2:4], byteorder='little', signed=True),
+             int.from_bytes(byts[4:6], byteorder='little', signed=True),
+             int.from_bytes(byts[6:8], byteorder='little', signed=True)]
+        self.w = list(map(lambda x: (x/32768), w))
+
+        self.temp = int.from_bytes(byts[8:10], byteorder='little', signed=True) /100
+
+        self.frame = byts
+
+    def __str__(self):
+        return "angular velocity=(wx:{:10.4f}, wy:{:10.4f}, wz:{:10.4f}), temp={:10.4f}".format(self.w[0], self.w[1], self.w[2], self.temp)
+
+class AtmosphericFrame:
+    def __init__(self, byts):
+        self.pa = int.from_bytes(byts[2:6], byteorder='little', signed=True)
+        self.height = int.from_bytes(byts[6:10], byteorder='little', signed=True)
+        self.frame = byts
+
+    def __str__(self):
+        return "Pa = {:10.4f}, Height = {:10.4f}".format(self.pa, self.height)
+
+FRAME_MAP = {
+    0x51 : AccelFrame,
+    0x52 : AngularVelocityFrame,
+    0x53 : AngleFrame,
+    0x54 : MagneticFrame,
+    0x56 : AtmosphericFrame,
+}
+
 class JY901:
     def __init__(self, source):
         self.source = source
@@ -28,11 +128,23 @@ class JY901:
             else:
                 self.bad_frame = self.bad_frame + 1
 
+    def next_frame(self):
+        while True:
+            f = self.read_frame()
+            if f[1] in FRAME_MAP:
+                return FRAME_MAP[f[1]](f)
+
     def next_accel(self):
         while True:
             f = self.read_frame()
             if f[1] == 0x51:
                 return AccelFrame(f)
+
+    def next_angle(self):
+        while True:
+            f = self.read_frame()
+            if f[1] == 0x53:
+                return AngleFrame(f)
 
     def set_baud_rate(self, baud):
         bs = bytearray()
@@ -110,21 +222,3 @@ class JY901:
         bs.append(v)
         bs.append(0x00)
         self.source.write(bs)
-
-class AccelFrame:
-    def __init__(self, byts):
-        a = [
-            int.from_bytes(byts[2:4], byteorder='little', signed=True),
-            int.from_bytes(byts[4:6], byteorder='little', signed=True),
-            int.from_bytes(byts[6:8], byteorder='little', signed=True)]
-        self.accel = list(map(lambda x: (x/32768)*16, a))
-        self.temp = int.from_bytes(byts[8:10], byteorder='little', signed=True) /100
-
-        self.frame = byts
-
-    def get_norm(self):
-        return numpy.linalg.norm(self.accel)
-
-    def __str__(self):
-        n = self.get_norm()
-        return "acc=({0:10.4f}, {1:10.4f}, {2:10.4f})~{3:10.4f}, temp={4:10.4f}".format(self.accel[0], self.accel[1], self.accel[2], n, self.temp)
--- test.py
+++ test.py
@@ -4,9 +4,101 @@ import argparse
 import serial
 
 import numpy as np
+
 import time
 
 from jy901 import JY901
+
+def draw2d(dev, interval, window_width):
+    import matplotlib.pyplot as plt
+    import matplotlib.animation as animation
+    from matplotlib import style
+    style.use('fivethirtyeight')
+
+
+    fig = plt.figure()
+    ax1 = fig.add_subplot(1,1,1)
+    xdata, ydata = ([],[])
+    start = time.time()
+
+    def animate(i):
+        global xdata,ydata
+        af = dev.next_accel()
+
+        y = af.get_norm()
+        print(af)
+        x = time.time() - start
+        xdata.append(x)
+        ydata.append(y)
+        if len(xdata) == window_width :
+            xdata = xdata[1:]
+            ydata = ydata[1:]
+        ax1.clear()
+        ax1.plot(xdata, ydata)
+
+    ani = animation.FuncAnimation(fig, animate, interval=interval)
+    plt.show()
+
+
+def draw3d(dev, interval, window_width):
+    import matplotlib.pyplot as plt
+    import mpl_toolkits.mplot3d.axes3d as p3
+    import matplotlib.animation as animation
+
+    # Attaching 3D axis to the figure
+    fig = plt.figure()
+    ax = p3.Axes3D(fig)
+
+    # Setting the axes properties
+    ax.set_xlim3d([0.0, 1.0])
+    ax.set_xlabel('X')
+
+    ax.set_ylim3d([0.0, 1.0])
+    ax.set_ylabel('Y')
+
+    ax.set_zlim3d([0.0, 1.0])
+    ax.set_zlabel('Z')
+
+    ax.set_title('Test')
+
+    x = np.array([[0.0,1],
+                  [0,0],
+                  [0,0]])
+    y = np.array([[0.0,0],
+                  [0,1],
+                  [0,0]])
+    z = np.array([[0.0,0],
+                  [0,0],
+                  [0,1]])
+
+    acc = np.array([[0.0, 0],
+                    [0.0, 0],
+                    [0.0, 0]])
+
+    # display sensor referencial
+    xp = ax.plot(x[0,0:2], x[1, 0:2], x[2, 0:2])[0]
+    yp = ax.plot(y[0,0:2], y[1, 0:2], y[2, 0:2])[0]
+    zp = ax.plot(z[0,0:2], z[1, 0:2], z[2, 0:2])[0]
+    accp = ax.plot(acc[0,0:2], acc[1, 0:2], acc[2, 0:2])[0]
+
+    def update_ref(num, ref, artists):
+        rot = dev.next_angle().get_rotation_matrix()
+        cur_acc = dev.next_accel().accel
+
+        ref[3][:,1] = cur_acc
+
+        for r,art in zip(ref, artists):
+            r = rot @ r
+            art.set_data(r[0:2, :2])
+            art.set_3d_properties(r[2, :2])
+
+        return artists
+
+    # Creating the Animation object
+    line_ani = animation.FuncAnimation(fig, update_ref, fargs=((x,y,z,acc),(xp,yp,zp,accp)), interval=interval)
+
+    plt.show()
+
 
 parser = argparse.ArgumentParser(description='CWT901CL utility.')
 
@@ -19,6 +111,9 @@ parser.add_argument('--rate', type=int, default=115200,
 parser.add_argument('--draw', action='store_true',
                     help='enable graphical display')
 
+parser.add_argument('--draw3d', action='store_true',
+                    help='enable 3D graphical display')
+
 parser.add_argument('--draw-interval', type=int, default=10,
                     help='interval between redraw of frame')
 
@@ -28,46 +123,17 @@ parser.add_argument('--draw-window', type=int, default=20,
 
 args = parser.parse_args()
 
-if args.draw:
-    import matplotlib.pyplot as plt
-    import matplotlib.animation as animation
-    from matplotlib import style
-    style.use('fivethirtyeight')
-
 ser = serial.Serial(args.device, args.rate)
-
 jy901_dev = JY901(ser)
 
-start = time.time()
-
 if args.draw:
-    fig = plt.figure()
-    ax1 = fig.add_subplot(1,1,1)
-    xdata, ydata = ([],[])
-    def animate(i):
-        global xdata,ydata
-        af = jy901_dev.next_accel()
-        y = af.get_norm()
-        print(af)
-        x = time.time() - start
-        xdata.append(x)
-        ydata.append(y)
-        if len(xdata) == args.draw_window :
-            xdata = xdata[1:]
-            ydata = ydata[1:]
-        ax1.clear()
-        ax1.plot(xdata, ydata)
-    ani = animation.FuncAnimation(fig, animate, interval=args.draw_interval)
-    plt.show()
-
+    draw2d(jy901_dev, args.draw_interval, args.draw_window)
+elif args.draw3d:
+    draw3d(jy901_dev, args.draw_interval, args.draw_window)
 else:
     while True:
-        af = jy901_dev.next_accel()
-        y = af.get_norm()
-        x = time.time() - start
-
-        # print("{0},{1}".format(x,y))
-        print(af)
+        f = jy901_dev.next_frame()
+        print(f)
 
 ser.close()
 
