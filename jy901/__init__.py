d0cf984f518da9d308c0ab0eea4d5d371024eed1
Marc Poulhiès <github@kataplop.net>
2018-04-15 21:08:10 +0200

    add basic docstrings

--- jy901/__init__.py
+++ jy901/__init__.py
@@ -5,7 +5,16 @@ import numpy as np
 import math
 
 class AccelFrame:
+    """
+    Describes an Acceleration frame.
+
+    `accel` field contains a 3D `np.array` with acceleration values.
+    `temp` field contains the temperature
+    """
+
     def __init__(self, byts):
+        """Returns a new AccelFrame object from the bytearray `byts`"""
+
         a = np.array([int.from_bytes(byts[2:4], byteorder='little', signed=True),
                       int.from_bytes(byts[4:6], byteorder='little', signed=True),
                       int.from_bytes(byts[6:8], byteorder='little', signed=True)])
@@ -17,6 +26,8 @@ class AccelFrame:
         self.frame = byts
 
     def get_norm(self):
+        """Returns the acceleration norm."""
+
         return numpy.linalg.norm(self.accel)
 
     def __str__(self):
@@ -25,7 +36,16 @@ class AccelFrame:
 
 
 class AngleFrame:
+    """
+    Describes an Angle frame.
+
+    `roll`, `pitch` and  `yaw` fields contains the respective angles.
+    `temp` contains the temperature
+    """
+
     def __init__(self, byts):
+        """Returns a new AngleFrame object from the bytearray `byts`"""
+
         self.roll = int.from_bytes(byts[2:4], byteorder='little', signed=True) / 32768
         self.pitch = int.from_bytes(byts[4:6], byteorder='little', signed=True) / 32768
         self.yaw = int.from_bytes(byts[6:8], byteorder='little', signed=True) / 32768
@@ -38,6 +58,8 @@ class AngleFrame:
         return "angle=(roll:{:10.4f}, pitch:{:10.4f}, yaw:{:10.4f}), temp={:10.4f}".format(self.roll, self.pitch, self.yaw, self.temp)
 
     def get_rotation_matrix(self):
+        """Returns the corresponding rotation matrix Z-Y-X."""
+
         yawMatrix = np.array([
             [math.cos(self.yaw), -math.sin(self.yaw), 0],
             [math.sin(self.yaw), math.cos(self.yaw), 0],
@@ -58,7 +80,16 @@ class AngleFrame:
         return yawMatrix @ pitchMatrix @ rollMatrix
 
 class MagneticFrame:
+    """
+    Describes a Magnetic frame.
+
+    `h` contains a 3D `np.array` with magnetic field values.
+    `temp` contains the temperature.
+    """
+
     def __init__(self, byts):
+        """Returns a new MagneticFrame from bytearray `byts`"""
+
         self.h = [int.from_bytes(byts[2:4], byteorder='little', signed=True),
                   int.from_bytes(byts[4:6], byteorder='little', signed=True),
                   int.from_bytes(byts[6:8], byteorder='little', signed=True)]
@@ -71,7 +102,15 @@ class MagneticFrame:
         return "magnetic H=({:10.4f}, {:10.4f}, {:10.4f}), temp={:10.4f}".format(self.h[0], self.h[1], self.h[2], self.temp)
 
 class AngularVelocityFrame:
+    """
+    Describes the Angular velocity frame.
+
+    `w` contains a 3D `np.array` with the angular velocity values.
+    `temp` contains the temperature
+    """
+
     def __init__(self, byts):
+        """Returns a new AngularVelocityFrame from bytearray `byts`"""
         w = [int.from_bytes(byts[2:4], byteorder='little', signed=True),
              int.from_bytes(byts[4:6], byteorder='little', signed=True),
              int.from_bytes(byts[6:8], byteorder='little', signed=True)]
@@ -85,7 +124,16 @@ class AngularVelocityFrame:
         return "angular velocity=(wx:{:10.4f}, wy:{:10.4f}, wz:{:10.4f}), temp={:10.4f}".format(self.w[0], self.w[1], self.w[2], self.temp)
 
 class AtmosphericFrame:
+    """
+    Describes the Atmospheric frame.
+
+    `pa` contains the atmospheric pressure in Pascal (Pa).
+    `height` contains the height in meters (m).
+    """
+
     def __init__(self, byts):
+        """Returns a new AtmosphericFrame object from bytearray `byts`"""
+
         self.pa = int.from_bytes(byts[2:6], byteorder='little', signed=True)
         self.height = int.from_bytes(byts[6:10], byteorder='little', signed=True)
         self.frame = byts
@@ -102,11 +150,19 @@ FRAME_MAP = {
 }
 
 class JY901:
+    """
+    Driver for JY901 device.
+    """
+
     def __init__(self, source):
+        """Returns a new JY901 device object. `source` must be read/write object."""
+
         self.source = source
         self.bad_frame = 0
 
     def read_frame(self):
+        """Returns the next valid frame as a bytearray read from the source."""
+
         while True:
             frame=bytearray()
 
@@ -129,24 +185,31 @@ class JY901:
                 self.bad_frame = self.bad_frame + 1
 
     def next_frame(self):
+        """Returns the next frame as a *Frame object."""
         while True:
             f = self.read_frame()
             if f[1] in FRAME_MAP:
                 return FRAME_MAP[f[1]](f)
 
     def next_accel(self):
+        """Returns the next AccelFrame read from the source."""
+
         while True:
             f = self.read_frame()
             if f[1] == 0x51:
                 return AccelFrame(f)
 
     def next_angle(self):
+        """Returns the next AngleFrame read from the source."""
+
         while True:
             f = self.read_frame()
             if f[1] == 0x53:
                 return AngleFrame(f)
 
     def set_baud_rate(self, baud):
+        """Sets the baudrate of the underlying JY901 device."""
+
         bs = bytearray()
         bs.append(0xFF)
         bs.append(0xAA)
@@ -170,6 +233,8 @@ class JY901:
         self.source.write(bs)
 
     def set_return_rate(self, hertz):
+        """Sets the return rate of the underlying JY901 device."""
+
         bs = bytearray()
         bs.append(0xFF)
         bs.append(0xAA)
@@ -194,6 +259,12 @@ class JY901:
         self.source.write(bs)
 
     def set_return_content(self, enabled):
+        """Sets the return content of the underlying device.
+        `enabled` must be a set containing the selected features from:
+        'time', 'acceleration', 'angular', 'angle', 'magnetic', 'atmo', 'latlon', 'gps', 'port'.
+        Beware that not all features are always available.
+        """
+
         bs = bytearray()
         bs.append(0xFF)
         bs.append(0xAA)
