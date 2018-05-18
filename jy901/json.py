74d0552d0b7a38cfbd868ca5b7a049a5688f0b39
Marc Poulhiès <github@kataplop.net>
2018-05-18 08:02:40 +0200

    Custom encoder for json dump

--- jy901/json.py
+++ jy901/json.py
@@ -2,11 +2,18 @@ import json
 import numpy as np
 
 from .frames import *
+import numpy as np
 
 class JSONEncoder(json.JSONEncoder):
 
     def default(self, obj):
-        if isinstance(obj, AccelFrame):
-            print("boom")
-            return None
+        # if hasattr(obj, '__json__'):
+        #     return obj.__json__()
+        # return json.JSONEncoder.default(self, obj)
+        if isinstance(obj, (AccelFrame, AngleFrame, MagneticFrame, AtmosphericFrame, AngularVelocityFrame)):
+            return obj.__dict__
+        elif isinstance(obj, np.ndarray):
+            return list(obj)
+        elif isinstance(obj, bytearray):
+            return [int(x) for x in obj]
         return json.JSONEncoder.default(self, obj)
